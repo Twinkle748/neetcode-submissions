d2f110f8f6ad10fb2ce85bde96b6958d42483c07
Twinkle748 <tahuja061@gmail.com>
2026-08-07 03:30:55 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+ bool canFinish(vector<int>& piles,int h , int mid){
+     int hours =0;
+     for(int pile : piles){
+        hours+=pile/mid;
+        if(pile%mid!=0) hours++;
+        if(hours>h) return false;
+     }
+     return true;
+ }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low =1;
+        int high = *max_element(piles.begin(),piles.end());
+        int ans = high;
+
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if(canFinish(piles,h, mid)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+      return ans;
+    }
+};
